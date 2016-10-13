3d3f4d064803a612c8dd100a0c6ba38a1f2ce69f
Tim Smith <tsmith@chef.io>
2016-10-13 11:18:14 +0100

    Swap a rubocop file for an inline rule disable
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/mixlib/versioning/versioning_spec.rb
+++ spec/mixlib/versioning/versioning_spec.rb
@@ -205,7 +205,7 @@ describe Mixlib::Versioning do
         build_versions: nil,
         prerelease_and_build_versions: "11.0.2-alpha.0+2013041116332",
       },
-      "11.0.2" => {
+      "11.0.2" => { # rubocop: disable Lint/DuplicatedKey
         releases_only: "11.0.2",
         prerelease_versions: "11.0.2-alpha.0",
         build_versions: nil,
